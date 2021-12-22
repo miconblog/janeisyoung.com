38d46bbf1ae43b8dc8228987f784885358f08d52
Byungdae Sohn <miconblog@gmai.com>
2021-12-22 23:20:53 +0900

    feat: Prisma 설정 및 샘플 추가

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true }
+  })
+  // use `console.dir` to print nested objects
+  console.dir(allUsers, { depth: null })
+
   res.status(200).json({ name: 'John Doe' })
 }
